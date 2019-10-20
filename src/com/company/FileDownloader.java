ba1b8e6707334768e87214b6c9921b61420b1417
pawelkon <erni.is.back@gmail.com>
2019-10-20 18:47:29 +0200

    the application downloads the file

---
+++ src/com/company/FileDownloader.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.net.URL;
+import java.io.FileOutputStream;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class FileDownloader {
+    
+    private final URL address;
+    
+    public FileDownloader(URL address) {
+        this.address = address;
+    }
+    
+    public void save(String path) {
+        try {
+            BufferedInputStream istream = new BufferedInputStream(address.openStream());
+            FileOutputStream ostream = new FileOutputStream(path);
+            
+            byte data[] = new byte[1024];
+            int byteContent;
+        
+            while ((byteContent = istream.read(data, 0, 1024)) != -1) {
+                ostream.write(data, 0, byteContent);
+            }
+            
+        } catch (IOException ex) {
+            Logger.getLogger(FileDownloader.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MalformedURLException {
+        FileDownloader downloaded = new FileDownloader(
+                new URL("https://s3.zylowski.net/public/input/2.txt")
+        );
+        downloaded.save("2.txt");
+        
         Loop loop = new Loop();
         loop.start();
     }
